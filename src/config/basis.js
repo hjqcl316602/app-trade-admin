fd6e0498c33d5a0c3621b6c356de446e38f458f9
hjqcl316602 <415877608@qq.com>
2020-02-17 20:18:29 +0800

    update

--- src/config/basis.js
+++ src/config/basis.js
@@ -1,18 +1,14 @@
-/*
- * @Description: In User Settings Edit
- * @Author: your name
- * @Date: 2019-09-04 20:44:28
- * @LastEditTime: 2019-09-04 20:46:14
- * @LastEditors: Please set LastEditors
- */
-
-const isTest = ['localhost', 'http://127.0.0.1'].findIndex(item => {
+const isTest = ["localhost", "http://127.0.0.1"].findIndex(item => {
   return window.location.origin.indexOf(item) > -1;
 });
 
 const image = {
-  compress: (isTest > -1 ? 'http://trade.bstchain.com' : window.location.origin) + '/res/upload/image/compress.do',
-  qr: (isTest > -1 ? 'http://trade.bstchain.com' : window.location.origin) + '/res/upload/image/qr.do' //
+  compress:
+    (isTest > -1 ? "http://trade.bstchain.com" : window.location.origin) +
+    "/p/upload/image/compress.do",
+  qr:
+    (isTest > -1 ? "http://trade.bstchain.com" : window.location.origin) +
+    "/p/upload/image/qr.do" //
 };
 
 export default {
